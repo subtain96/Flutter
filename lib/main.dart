90883cc0acc19b36cdd63e4d8cb5107ad56cce6a
subtain96 <awaisiofficial0096@gmail.com>
2025-05-10 06:57:27 -0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp (const myApp());
+}
+class myApp extends StatelessWidget {
+  const myApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
